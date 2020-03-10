a2c275b2251cfd511a741d2e5f518334c9a51caf
rolemadelen <rolemadelen17@pm.me>
2020-03-10 15:00:23 -0700

    palindrome finished

--- frontend/palindrome/script.js
+++ frontend/palindrome/script.js
@@ -1,15 +1,16 @@
-inputBox = document.getElementById('palindrome-input');
-bgColor =  document.getElementById('bg-color');
+'use strict';
 
-console.log(bgColor);
+let inputBox = document.getElementById('palindrome-input');
+let bgColor =  document.getElementById('bg-color');
+let resultBox = document.getElementById('result');
 
 function checkPalindrome(str) {
     str = str.replace(/\s/g, '');
     let size = str.length;
-    let mid = size << 1;
+    let mid = size >> 1;
 
     for (let i=0; i<mid; ++i) {
-        if (str[i] != str[size - i - 1])
+        if (str[i].toLowerCase() != str[size - i - 1].toLowerCase())
             return false;
     }
 
@@ -19,9 +20,12 @@ function checkPalindrome(str) {
 inputBox.onkeyup = function() {
     if (inputBox.value.length == 0) {
         bgColor.style.backgroundColor = '#fff';
+        resultBox.textContent = "";
     } else if (checkPalindrome(inputBox.value) === true) {
         bgColor.style.backgroundColor = '#0f0';
+        resultBox.textContent = "입니다";
    } else {
         bgColor.style.backgroundColor = '#f00';
+        resultBox.textContent = "이 아닙니다";
     }
 }
