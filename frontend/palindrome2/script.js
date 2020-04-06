a13bc6332900f29db06b55d9f7cc8150319eb930
Blue Eu <euisblue@pm.me>
2020-04-06 14:58:40 +0900

    palindrome2 i.p.

---
+++ frontend/palindrome2/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let palindromeTextBox = document.getElementById('palindrome-textbox');
+let palindromeCheckButton = document.getElementById('palindrome-check-button');
+
+console.log(palindromeTextBox);
+console.log(palindromeCheckButton);
