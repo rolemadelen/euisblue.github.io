19202188197de1faa1fb2c08adb1cc82302fc9ed
Jii <jione.eu@gmail.com>
2020-05-06 21:22:36 +0900

    frontend repo removed

--- frontend/palindrome2/script.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-let palindromeTextBox = document.getElementById('palindrome-textbox');
-let palindromeCheckButton = document.getElementById('palindrome-check-button');
-
-console.log(palindromeTextBox);
-console.log(palindromeCheckButton);
