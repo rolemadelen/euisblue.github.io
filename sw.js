a34a8b43d1c7f0b252471b06307474e6ac3fac7a
Blue Eu <euisblue@pm.me>
2021-05-22 19:22:38 +0900

    Add: aos

--- sw.js
+++ sw.js
@@ -23,9 +23,6 @@ const PRECACHE_LIST = [
   "./img/404-bg.jpg",
   "./css/ipflegen-blog.min.css",
   "./css/bootstrap.min.css"
-  // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css",
-  // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/fonts/fontawesome-webfont.woff2?v=4.6.3",
-  // "//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
